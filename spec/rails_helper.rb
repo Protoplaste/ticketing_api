553d3e3591abd2e3a0a4b204fb2814cdf20a2e4f
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-01-30 07:22:20 +0100

    adding request spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 require 'database_cleaner'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -40,6 +41,7 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.include RequestSpecHelper, type: :request
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
