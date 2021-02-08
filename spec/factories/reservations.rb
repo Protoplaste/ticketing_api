4bdd9c05eb717407a154ed92067da8377ef48fa1
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-08 09:12:49 +0100

    update factories

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -2,11 +2,15 @@ FactoryBot.define do
   factory :reservation do
     status { "unpaid" }
     factory :full_reservation do
-      after :create do |reservation|
-        seats = create_list(:seat, 3, event: create(:event))
+      before :create do |reservation|
+        sector = create(:sector)
+        seats = create_list(:seat, 3, sector: sector)
         seats.each do |seat|
-          create(:ticket, seat: seat, reservation: reservation)
+          reservation.tickets << Ticket.new(seat: seat)
         end
+      end
+
+      after :create do |reservation|
         create(:payment, reservation: reservation, amount: reservation.total_cost)
       end
     end
