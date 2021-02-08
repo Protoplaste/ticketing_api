fee5a11d08df8cca03899acd1499dfa43a08f2d0
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-08 09:11:24 +0100

    add validations for selling options to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,36 @@ class Reservation < ApplicationRecord
   has_many :seats, through: :tickets
   has_one :payment
 
+  validates :tickets, presence: true
+  validate :even_ticket_number, if: -> { sector&.selling_option_even }
+  validate :avoids_one_seat_left, if: -> { sector&.selling_option_avoid_one }
+  validate :seats_next_to_each_other, if: -> { sector&.selling_option_all_together }
+
   def total_cost
     seats.map(&:cost).reduce(:+)
+  end
+
+  def sector
+    tickets.first&.sector
+  end
+
+  private
+
+  def even_ticket_number
+    if tickets.length.odd?
+      errors.add(:ticket_count, "cannot be an odd number in this sector")
+    end
+  end
+
+  def avoids_one_seat_left
+    if (sector.seats.count - tickets.length == 1)
+      errors.add(:ticket_count, "cannot leave one seat empty in this sector")
+    end
+  end
+
+  def seats_next_to_each_other
+    if seats.map(&:row).uniq.length > 1
+      errors.add(:ticket_count, "cannot reserve seats in different rows in this sector")
+    end
   end
 end
