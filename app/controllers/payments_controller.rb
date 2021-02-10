16a0db964672165ba4ce48a85321f4819fdeb06e
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-10 08:17:33 +0100

    automaticly attampting to refund payment if it was made after reservation timed out

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,7 +9,14 @@ class PaymentsController < ApplicationController
     payment.set_paid
     payment.save!
 
+    if payment.reservation.status == "timeout"
+      response = Adapter::Payment::Gateway.refund(token: params[:token],
+                                                  amount: payment.amount,
+                                                  payment_id: payment.id)
+      payment.set_refunded
+      payment.save!
+    end
+
     json_response(response, :ok)
   end
-
 end
