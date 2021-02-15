a27be84134882d8fd0f1c770df949ceda7bd51a2
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-15 17:44:48 +0100

    adding missing sector factory

--- spec/factories/sectors.rb
+++ spec/factories/sectors.rb
@@ -5,5 +5,10 @@ FactoryBot.define do
     selling_option_all_together { false }
     selling_option_avoid_one { false }
     event
+    factory :sector_with_selling_options do
+      selling_option_even { true }
+      selling_option_all_together { true }
+      selling_option_avoid_one { true }
+    end
   end
 end
