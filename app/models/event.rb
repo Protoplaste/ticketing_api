233b787600fc46d8c5f14bed40e48b6f5a6841f8
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-15 17:43:22 +0100

    minor fixes and refactors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,3 @@
 class Event < ApplicationRecord
-  has_many :seats
+  has_many :sectors
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,9 +4,9 @@ class Reservation < ApplicationRecord
   has_one :payment
 
   validates :tickets, presence: true, on: :create
-  validate :even_ticket_number, if: -> { sector&.selling_option_even }
-  validate :avoids_one_seat_left, if: -> { sector&.selling_option_avoid_one }
-  validate :seats_next_to_each_other, if: -> { sector&.selling_option_all_together }
+  validate :even_ticket_number, if: -> { selling_option_even? }
+  validate :avoids_one_seat_left, if: -> { selling_option_avoid_one? }
+  validate :seats_next_to_each_other, if: -> { selling_option_all_together? }
 
   def total_cost
     seats.map(&:cost).reduce(:+)
@@ -18,16 +18,28 @@ class Reservation < ApplicationRecord
 
   private
 
+  def selling_option_even?
+    sector&.selling_option_even 
+  end
+
   def even_ticket_number
     if tickets.length.odd?
       errors.add(:ticket_count, "cannot be an odd number in this sector")
     end
   end
 
+  def selling_option_avoid_one?
+    sector&.selling_option_avoid_one
+  end
+
   def avoids_one_seat_left
     if (sector.seats.count - tickets.length == 1)
       errors.add(:ticket_count, "cannot leave one seat empty in this sector")
     end
+  end
+
+  def selling_option_all_together?
+    sector&.selling_option_all_together
   end
 
   def seats_next_to_each_other
