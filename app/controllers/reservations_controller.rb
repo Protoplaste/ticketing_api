8f87e9f7c80edbd3c9a327159dcdae7cf2482cc6
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-16 15:02:34 +0100

    refactoring reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,17 +6,21 @@ class ReservationsController < ApplicationController
       # Find seats that user wants to reserve
       seats = Seat.includes(:sector).find(params[:seats])
 
-      # Create the reservation for those seats
+
+      # Build a reservation with those seats
       @reservation = Reservation.new
       seats.each do |seat|
         @reservation.tickets << Ticket.new(seat: seat)
       end
-      @reservation.save!
 
-      # Prepare payment
-      Payment.create!(reservation: @reservation, amount: @reservation.total_cost)
-      # Create a job to remove the reservation in 15 minutes if it's not paid for
-      ReservationTimeoutJob.set(wait: 15.minutes).perform_later(@reservation)
+      @reservation.transaction do
+        @reservation.save!
+
+        # Prepare payment
+        Payment.create!(reservation: @reservation, amount: @reservation.total_cost)
+        # Create a job to remove the reservation in 15 minutes if it's not paid for
+        ReservationTimeoutJob.set(wait: 15.minutes).perform_later(@reservation)
+      end
     end
 
     json_response(@reservation, :created)
