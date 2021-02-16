523d8d3bf10ac4a62bc5dfcd6a3fb62e10a6b848
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-16 18:50:46 +0100

    refactoring payments controller with rubocop

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,16 +4,14 @@ class PaymentsController < ApplicationController
   require 'payment_adapter/gateway'
   include GatewayError
 
+  before_action :fetch_payment, only: %i[charge refund]
+
   def charge
-    payment = Payment.find(params[:id])
-
-    response = Adapter::Payment::Gateway.charge(token: params[:token], amount: payment.amount)
-
-    payment.set_paid
-    payment.save!
+    response = Adapter::Payment::Gateway.charge(token: params[:token], amount: @payment.amount)
+    process_payment
 
     # Checking for reservation status after processing payment to avoid racing condition with reservation timeout job
-    if payment.reservation.status == 'timeout'
+    if @payment.reservation.status == 'timeout'
       redirect_to :refund
     else
       json_response(response, :ok)
@@ -21,19 +19,29 @@ class PaymentsController < ApplicationController
   end
 
   def refund
-    payment = Payment.includes(reservation: [:tickets]).find(params[:id])
+    raise PaymentUnrefundable, @payment.status unless %w[paid refund_failed].include? @payment.status
 
-    if %w[paid refund_failed].include? payment.status
-      response = Adapter::Payment::Gateway.refund(token: params[:token],
-                                                  amount: payment.amount,
-                                                  payment_id: payment.id)
-      payment.set_refunded
-      payment.save!
-      payment.reservation.tickets.each(&:destroy)
+    response = Adapter::Payment::Gateway.refund(token: params[:token],
+                                                amount: @payment.amount,
+                                                payment_id: @payment.id)
+    process_refund
+    json_response(response, :ok)
+  end
 
-      json_response(response, :ok)
-    else
-      raise PaymentUnrefundable, payment.status
-    end
+  private
+
+  def fetch_payment
+    @payment = Payment.includes(reservation: [:tickets]).find(params[:id])
+  end
+
+  def process_payment
+    @payment.set_paid
+    @payment.save!
+  end
+
+  def process_refund
+    @payment.set_refunded
+    @payment.save!
+    @payment.reservation.tickets.each(&:destroy)
   end
 end
