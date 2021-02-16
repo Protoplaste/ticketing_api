619124aa781cfbd298ed9dcb5705d54cd12eccb6
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-16 18:51:04 +0100

    refactoring reservations controller with rubocop

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,27 +1,34 @@
 # frozen_string_literal: true
 
 class ReservationsController < ApplicationController
+  before_action :fetch_seats, only: [:create]
+
   def create
-    ActiveRecord::Base.transaction do
-      # Find seats that user wants to reserve
-      seats = Seat.includes(:sector).find(params[:seats])
-
-      # Build a reservation with those seats
-      @reservation = Reservation.new
-      seats.each do |seat|
-        @reservation.tickets << Ticket.new(seat: seat)
-      end
-
-      @reservation.transaction do
-        @reservation.save!
-
-        # Prepare payment
-        Payment.create!(reservation: @reservation, amount: @reservation.total_cost)
-        # Create a job to remove the reservation in 15 minutes if it's not paid for
-        ReservationTimeoutJob.set(wait: 15.minutes).perform_later(@reservation)
-      end
+    # Build a reservation for seats
+    @reservation = Reservation.new
+    @seats.each do |seat|
+      @reservation.tickets << Ticket.new(seat: seat)
     end
 
+    save_reservation
+
     json_response(@reservation, :created)
+  end
+
+  private
+
+  def fetch_seats
+    @seats = Seat.includes(:sector).find(params[:seats])
+  end
+
+  def save_reservation
+    @reservation.transaction do
+      @reservation.save!
+
+      # Prepare payment
+      Payment.create!(reservation: @reservation, amount: @reservation.total_cost)
+      # Create a job to remove the reservation in 15 minutes if it's not paid for
+      ReservationTimeoutJob.set(wait: 15.minutes).perform_later(@reservation)
+    end
   end
 end
