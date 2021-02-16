615e9628c81e17b498fb83358fcfc0b1cbe0f6b1
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-16 18:51:34 +0100

    refactoring reservation time out job with rubocop

--- app/jobs/reservation_timeout_job.rb
+++ app/jobs/reservation_timeout_job.rb
@@ -4,9 +4,9 @@ class ReservationTimeoutJob < ApplicationJob
   queue_as :default
 
   def perform(reservation)
-    if reservation.payment.status != 'paid'
-      reservation.update_attribute(:status, 'timeout')
-      reservation.tickets.each(&:destroy!)
-    end
+    return unless reservation.payment.status != 'paid'
+
+    reservation.update(status: 'timeout')
+    reservation.tickets.each(&:destroy!)
   end
 end
