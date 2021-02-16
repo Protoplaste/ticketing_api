98315c7dbda3ead71c9e2f00be437e138b332bbf
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-16 15:03:08 +0100

    drying reservation unit specs

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe Reservation, type: :model do
   it { is_expected.to validate_presence_of(:tickets) }
 
   context 'selling options validations' do
+    error_messages = { odd_seat_number: 'Ticket count cannot be an odd number in this sector',
+                       one_seat_left_empty: 'Ticket count cannot leave one seat empty in this sector',
+                       seats_separated: 'Seat placement cannot reserve seats in different rows in this sector' }
     # Prepare reservation that fails all selling option validations
     subject { build(:full_reservation, seats: seats) }
 
@@ -28,16 +31,10 @@ RSpec.describe Reservation, type: :model do
 
     context 'when selling options are enabled' do
       context "and reservation doesn't meet them" do
-        it 'adds error message when ticket count is not even' do
-          expect(subject.errors.full_messages).to include 'Ticket count cannot be an odd number in this sector'
-        end
-
-        it 'adds error message when reservation would leave one seat empty' do
-          expect(subject.errors.full_messages).to include 'Ticket count cannot leave one seat empty in this sector'
-        end
-
-        it 'adds error message when seats are in different rows' do
-          expect(subject.errors.full_messages).to include 'Seat placement cannot reserve seats in different rows in this sector'
+        error_messages.each do |key, message|
+          it "adds error message for #{key}" do
+            expect(subject.errors.full_messages).to include message
+          end
         end
       end
 
@@ -46,16 +43,10 @@ RSpec.describe Reservation, type: :model do
         let(:rows) { %w[A A] }
         let(:additional_seat) {}
 
-        it 'does not add error message when ticket count is not even' do
-          expect(subject.errors.full_messages).not_to include 'Ticket count cannot be an odd number in this sector'
-        end
-
-        it 'does not add error message when reservation would leave one seat empty' do
-          expect(subject.errors.full_messages).not_to include 'Ticket count cannot leave one seat empty in this sector'
-        end
-
-        it 'does not add error message when seats are in different rows' do
-          expect(subject.errors.full_messages).not_to include 'Seat placement cannot reserve seats in different rows in this sector'
+        error_messages.each do |key, message|
+          it "does not add error message for #{key}" do
+            expect(subject.errors.full_messages).to_not include message
+          end
         end
       end
     end
@@ -63,16 +54,10 @@ RSpec.describe Reservation, type: :model do
     context 'when selling options are disabled' do
       let(:sector) { create(:sector) }
 
-      it 'does not validate the seat number is even' do
-        expect(subject.errors.full_messages).not_to include 'Ticket count cannot be an odd number in this sector'
-      end
-
-      it "does not validate the reservation won't leave one seat empty" do
-        expect(subject.errors.full_messages).not_to include 'Ticket count cannot leave one seat empty in this sector'
-      end
-
-      it 'does not validate that seats are next to each other' do
-        expect(subject.errors.full_messages).not_to include 'Seat placement cannot reserve seats in different rows in this sector'
+      error_messages.each do |key, message|
+        it "does not add error message for #{key}" do
+          expect(subject.errors.full_messages).to_not include message
+        end
       end
     end
   end
