95b97ad2e082d4924569e05c0c623c4be3b9d4fd
Mateusz Pietrzyk <mateusz.p.pietrzyk@gmail.com>
2021-02-16 14:49:35 +0100

    adding spec for payment refund process

--- spec/requests/payments_request_spec.rb
+++ spec/requests/payments_request_spec.rb
@@ -3,16 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Payments', type: :request do
-  let!(:reservation) { create(:full_reservation) }
+  let(:reservation) { create(:reservation_with_tickets) }
+  let(:status) { 'pending' }
+  let!(:payment) { create(:payment, reservation: reservation, status: status) }
+  let(:payment_id) { payment.id }
+  let(:token) { 'valid' }
+  let(:params) { { token: token } }
 
-  describe 'put /payment/:payment_id' do
-    let(:token) { 'valid' }
-    let(:payment_id) { reservation.payment.id }
-    let(:params) { { token: token } }
+  describe 'put /payment/:payment_id/charge' do
 
-    before { put "/payments/#{payment_id}", params: params }
+    subject { put "/payments/#{payment_id}/charge", params: params }
 
     context 'when payment goes through' do
+      before { subject }
+
       it 'returns correct amount paid' do
         expect(json['amount']).to eq reservation.total_cost
       end
@@ -29,20 +33,18 @@ RSpec.describe 'Payments', type: :request do
     context 'when reservation timed out during payment' do
       before do
         ReservationTimeoutJob.perform_now(reservation)
-        put "/payments/#{payment_id}", params: params
+        subject
       end
 
-      it 'processes a refund' do
-        expect(json['transaction_type']).to eq 'refund'
-      end
-
-      it 'sets payment status to refunded' do
-        expect(Payment.last.status).to eq 'refunded'
+      it 'redirects to refund process' do
+        expect(subject).to redirect_to(action: :refund, id: payment_id)
       end
     end
 
     context 'when payment not found' do
       let(:payment_id) { 0 }
+
+      before { subject }
 
       it 'returns an error' do
         expect(json['message']).to include("Couldn't find Payment")
@@ -50,6 +52,8 @@ RSpec.describe 'Payments', type: :request do
     end
 
     context 'when payment fails' do
+      before { subject }
+
       context 'due to card error' do
         let(:token) { 'card_error' }
 
@@ -80,6 +84,52 @@ RSpec.describe 'Payments', type: :request do
         it 'returns status code 400' do
           expect(response).to have_http_status(:bad_request)
         end
+      end
+    end
+  end
+
+  describe 'put /payment/:payment_id/refund' do
+    let(:status) { 'paid' }
+
+    subject { put "/payments/#{payment_id}/refund", params: params }
+
+    before { subject }
+
+    context 'when refund request is valid' do
+      include_examples 'payment request refund success examples'
+    end
+
+    context 'when payment was not finalized' do
+      let(:status) { 'pending' }
+      include_examples 'payment refund request failure due to status examples'
+    end
+
+    context 'when payment was already refunded' do
+      let(:status) { 'refunded' }
+      include_examples 'payment refund request failure due to status examples'
+    end
+
+    context 'when payment refund failed before' do
+      let(:status) { 'refund_failed' }
+      include_examples 'payment request refund success examples'
+    end
+
+    context 'when payment gateway fails' do
+      let(:token) { :refund_error }
+      it 'changes payments status' do
+        expect(Payment.last.status).to eq 'refund_failed'
+      end
+
+      it 'does not remove reservations' do
+        expect(Payment.last.reservation.tickets.empty?).not_to be true
+      end
+
+      it 'returns correct error message' do
+        expect(json['message']).to eq 'Something went wrong with the refund process'
+      end
+
+      it 'returns status code 400' do
+        expect(response).to have_http_status(:bad_request)
       end
     end
   end
---
+++ spec/support/shared_examples/payment_request_examples.rb
@@ -0,0 +1,35 @@
+RSpec.shared_examples 'payment request refund success examples' do
+  it 'changes payment status' do
+    expect(Payment.last.status).to eq 'refunded'
+  end
+
+  it 'removes reservations' do
+    expect(Payment.last.reservation.tickets.empty?).to be true
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'returns correct amount refunded' do
+    expect(json['amount']).to eq reservation.total_cost
+  end
+end
+
+RSpec.shared_examples 'payment refund request failure due to status examples' do
+  it 'does not change payment status' do
+    expect(Payment.last.status).to eq status
+  end
+
+  it 'does not remove reservations' do
+    expect(Payment.last.reservation.tickets.empty?).not_to be true
+  end
+
+  it 'returns correct error message' do
+    expect(json['message']).to eq "Cannot refund payment with status #{status}"
+  end
+
+  it 'returns status code 400' do
+    expect(response).to have_http_status(:bad_request)
+  end
+end
